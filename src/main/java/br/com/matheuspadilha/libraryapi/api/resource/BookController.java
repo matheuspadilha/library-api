923cc1d288433480e0c37e473a5774ad9c4072e5
Matheus Padilha <mvpadilha94@gmail.com>
2022-01-03 20:53:53 -0300

    adicionando validacao ao cadastrar um livro e adicionando lib de bean validation

--- src/main/java/br/com/matheuspadilha/libraryapi/api/resource/BookController.java
+++ src/main/java/br/com/matheuspadilha/libraryapi/api/resource/BookController.java
@@ -1,11 +1,18 @@
 package br.com.matheuspadilha.libraryapi.api.resource;
 
 import br.com.matheuspadilha.libraryapi.api.dto.BookDTO;
+import br.com.matheuspadilha.libraryapi.api.exception.ApiErrors;
 import br.com.matheuspadilha.libraryapi.model.entity.Book;
 import br.com.matheuspadilha.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -21,10 +28,18 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(@RequestBody BookDTO dto){
+    public BookDTO create(@RequestBody @Valid BookDTO dto){
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
 
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+
+        return new ApiErrors(bindingResult);
     }
 }
--- src/test/java/br/com/matheuspadilha/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/br/com/matheuspadilha/libraryapi/api/resource/BookControllerTest.java
@@ -3,7 +3,9 @@ package br.com.matheuspadilha.libraryapi.api.resource;
 import br.com.matheuspadilha.libraryapi.api.dto.BookDTO;
 import br.com.matheuspadilha.libraryapi.model.entity.Book;
 import br.com.matheuspadilha.libraryapi.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -65,7 +68,20 @@ class BookControllerTest {
 
     @Test
     @DisplayName("Deve lançar erro de validação quando não houver dados suficientes para criação do livro.")
-    void createInvalidBookTest() {
+    void createInvalidBookTest() throws Exception {
+        //cenario
+        String json = new ObjectMapper().writeValueAsString(new BookDTO());
 
+        // execucao
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        // verificacao
+        mvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(3)));
     }
 }
