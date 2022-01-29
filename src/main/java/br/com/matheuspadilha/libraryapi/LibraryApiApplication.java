36908f9d6c15a2fe5746d412e503aab0304e694e
Matheus Padilha <mvpadilha94@gmail.com>
2022-01-28 21:39:22 -0300

    implementando swagger 3

--- src/main/java/br/com/matheuspadilha/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/matheuspadilha/libraryapi/LibraryApiApplication.java
@@ -1,5 +1,8 @@
 package br.com.matheuspadilha.libraryapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +11,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(info = @Info(
+		title = "Library API",
+		description = "API do Projeto de controle de aluguel de livros",
+		version = "1.0",
+		contact = @Contact(
+				name = "Matheus Padilha",
+				url = "https://github.com/matheuspadilha",
+				email = "contato@matheuspadilha.com.br"
+		)
+))
 public class LibraryApiApplication {
 
 	@Bean
